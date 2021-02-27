33f4297a6e9b09c57ff2cdc5610ca0a601e53d8c
Jheferson Renan Borges <jheferson@asaplog.com.br>
2021-02-27 13:19:53 -0300

    nlw4 extra mile

--- lib/rocketpay/accounts/transaction.ex
+++ lib/rocketpay/accounts/transaction.ex
@@ -2,7 +2,7 @@ defmodule Rocketpay.Accounts.Transaction do
 
   alias Ecto.Multi
 
-  alias Rocketpay.Repo
+  alias Rocketpay.{Account, Repo, User}
   alias Rocketpay.Accounts.Operation
   alias Rocketpay.Accounts.Transaction.Response, as: TransactionResponse
 
@@ -13,17 +13,31 @@ defmodule Rocketpay.Accounts.Transaction do
     Multi.new()
     |> Multi.merge(fn _changes -> Operation.call(withdraw_params, :withdraw) end)
     |> Multi.merge(fn _changes -> Operation.call(deposit_params, :deposit) end)
-    |> run_transaction()
+    |> run_transaction(value)
   end
 
   defp build_params(id, value), do: %{"id" => id, "value" => value}
 
-  defp run_transaction(multi) do
+  defp run_transaction(multi, value) do
     case Repo.transaction(multi) do
       {:error, _operation, reason, _changes} -> {:error, reason}
       {:ok, %{deposit: to_account, withdraw: from_account}} ->
+        Task.start(fn -> print_proof(from_account, to_account, value) end)
         {:ok, TransactionResponse.build(from_account, to_account)}
     end
+  end
+
+  defp print_proof(from_account, to_account, value) do
+    nick_from = get_nickname(from_account)
+    nick_to = get_nickname(to_account)
+    prof_text = "Transaction from #{nick_from} to #{nick_to} value #{value}"
+    File.write!("./.proof/prof.txt", prof_text)
+  end
+
+
+  defp get_nickname(account) do
+    user = Repo.preload(account, :user).user
+    user.nickname
   end
 
 end
