310a478cd685581780973851cca079f703bc7de1
Jheferson Renan Borges <jheferson@asaplog.com.br>
2021-02-27 00:57:17 -0300

    Fim da nlw4

---
+++ test/rocketpay/users/create_test.exs
@@ -0,0 +1,44 @@
+defmodule Rocketpay.Users.CreateTest do
+  use Rocketpay.DataCase, async: true
+
+  alias Rocketpay.User
+  alias Rocketpay.Users.Create
+
+  describe "call/1" do
+    test "when all params are valid, return an use" do
+      params = %{
+        name: "Rafael",
+        password: "123456",
+        nickname: "camarda",
+        email: "rafael@banana",
+        age: 27
+      }
+
+
+      {:ok, %User{id: user_id}} = Create.call(params)
+      user = Repo.get(User, user_id)
+
+      assert %User{name: "Rafael", age: 27, id: ^user_id} = user
+    end
+
+    test "when there are invalid params , return an error" do
+      params = %{
+        name: "Rafael",
+        nickname: "camarda",
+        email: "rafael@banana",
+        age: 15
+      }
+
+
+      {:error, changeset} = Create.call(params)
+
+      expected_response = %{
+        age: ["must be greater than or equal to 18"],
+        password: ["can't be blank"]
+      }
+
+      assert errors_on(changeset) == expected_response
+    end
+
+  end
+end
